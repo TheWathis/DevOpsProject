450ce4309ea6530b87696380a05beceb37e2321f
anthony <thonymartiinez@gmail.com>
2021-04-07 11:12:29 +0200

    Ajout d'un test de base, mvn test fonctionnel

--- Pandas/src/main/java/Dataframe.java
+++ Pandas/src/main/java/Dataframe.java
@@ -1,10 +1,11 @@
 public class Dataframe {
 	
-	public int marche() {
-		return 1;
-	}
-	
     public static void main(String args[]) {
         System.out.println("coucou je fonctionne ");
+    }
+
+    public int marche() {
+        return 1;
+
     }
 }
