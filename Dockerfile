e47bbffdeb531a05a83e0cd776d5d85b8e349e6a
Ludovic Lang <ludovic.lang7@gmail.com>
2021-04-28 18:14:55 +0200

    add function in docker image

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ COPY . /usr/src/myapp/
 WORKDIR /usr/src/myapp/
 RUN apk --no-cache add maven && mvn --version
 RUN mvn package
-#RUN javac -cp Pandas/target/Pandas-1.0-SNAPSHOT.jar ExampleDataframe.java
+RUN javac -cp Pandas/target/Pandas-1.0-SNAPSHOT.jar docker/ExampleDataframe.java
 
-#CMD java -cp  Pandas/target/Pandas-1.0-SNAPSHOT.jar:.  ExampleDataframe
+CMD java -cp  Pandas/target/Pandas-1.0-SNAPSHOT.jar:docker  ExampleDataframe
--- docker/ExampleDataframe.java
+++ docker/ExampleDataframe.java
@@ -1,8 +1,20 @@
-import Pandas.Dataframe;
+import pandas.Dataframe;
 
 public class ExampleDataframe {
     public static void main(String args[]) {
-            Dataframe df = new Dataframe("fichier_test.csv");
-            df.printDataframe();
+        Dataframe df = new Dataframe("docker/fichier_test.csv");
+        System.out.println(df);
+        try {
+            System.out.println("meanValueByIndex(2) :");
+            System.out.println(df.meanValueByIndex(2));
+            System.out.println("\nmeanValueByLabel(Année de naissance) :");
+            System.out.println(df.meanValueByLabel("Année de naissance"));
+            System.out.println("\nminValueByLabel(Année de naissance) :");
+            System.out.println(df.minValueByLabel("Année de naissance"));
+            
+        } catch (Exception e) { 
+            e.printStackTrace();
+        }
+        
     }
 }
